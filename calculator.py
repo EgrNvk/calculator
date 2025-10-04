6ae18f060d8cce74770e28e008ade8767b9f3bd7
EgrNvk <104683701+EgrNvk@users.noreply.github.com>
2025-10-04 16:27:03 +0300

    add main code
    Додав основну програму

--- calculator.py
+++ calculator.py
@@ -1,3 +1,6 @@
+from asyncio import Event
+
+
 class Calculator:
     def __init__(self, number1, number2):
         self.number1 = number1
@@ -23,3 +26,15 @@ class Calculator:
         print(f"{self.number1} / {self.number2} = {result}")
         return
 
+number1=float(input("Введіть перше число - "))
+number2=float(input("Введіть друге число - "))
+
+action=input("Введіть потрібну дію (+, -, *, /) ")
+if action == "+":
+    Calculator(number1,number2).addition()
+elif action == "-":
+    Calculator(number1,number2).subtraction()
+elif action == "*":
+    Calculator(number1,number2).multiplication()
+elif action == "/":
+    Calculator(number1,number2).division()
